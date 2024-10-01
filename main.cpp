3b710a0e9620beb23d7eef9d48533bc1ddb25bca
sakamoto-ko <115054832+sakamoto-ko@users.noreply.github.com>
2024-10-01 15:21:04 +0900

    02_03

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
-	printf("Hello,World!\n");
+int main(int argc, char* argv[]) {
+
+	//argcの数だけ繰り返す
+	for (int i = 0; i < argc; i++) {
+		//文字列argvのi番を表示
+		printf(argv[i]);
+		printf("\n");
+	}
 
 	system("pause");
 
