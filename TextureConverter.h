950ab8549a10612dc02431aba4a6e1c7687af0ca
sakamoto-ko <115054832+sakamoto-ko@users.noreply.github.com>
2024-10-24 15:12:52 +0900

    error

--- TextureConverter.h
+++ TextureConverter.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include "externals/DirectXTex/DirectXTex.h"
 
 class TextureConverter
 {
@@ -12,5 +13,22 @@ private:
 	void LoadWICTextureFromFile(const std::string& filePath);
 	//マルチバイト文字列をワイド文字列に変換
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+	//フォルダパスとファイル名を分離する
+	void SeparateFilePath(const std::wstring& filePath);
+	//DDSテクスチャとしてファイル書き出し
+	void SaveDDSTextureToFile();
+
+private:
+	//画像の情報
+	DirectX::TexMetadata metadata_;
+	//画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+
+	//ディレクトリパス
+	std::wstring directoryPath_;
+	//ファイル名
+	std::wstring fileName_;
+	//ファイル拡張子
+	std::wstring fileExt_;
 };
 
